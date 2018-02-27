db26d60ef01c481bd0e90126cc22753317aa2372
venu.mora <venukumar.mora@gmail.com>
2018-02-27 02:34:29 -0800

    Fix issue with initial setup

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
-    res.render("index", hbsObject);
+    res.render("index");
 });
 
 // Export routes for server.js to use.
