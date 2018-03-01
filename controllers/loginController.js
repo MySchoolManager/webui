9688abf4b67d93872004b20d2ca645ac31ec2e5b
venu.mora <venukumar.mora@gmail.com>
2018-03-01 00:44:28 -0800

    F-0003 adding semantic UI to project, added sign in page UI

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -7,5 +7,13 @@ router.get("/", function(req, res) {
     res.render("index");
 });
 
+router.get("/signup/user", function(req, res) {
+    res.render("signupuser");
+});
+
+router.get("/signup/school", function(req, res) {
+    res.render("signupschool");
+});
+
 // Export routes for server.js to use.
 module.exports = router;
