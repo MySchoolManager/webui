03e01371ccdcff85b4acae15a53bc071e4bb53a0
venu.mora <venukumar.mora@gmail.com>
2018-03-04 07:08:39 -0800

    Initial rotues and files for home controller

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -3,4 +3,36 @@ module.exports = function(app, firebaseInstance) {
   app.get('/home', function(req, res) {
     res.render('home');
   });
+
+  app.get('/school', function(req, res) {
+    res.render('school/readonlyschool');
+  });
+
+  app.get('/edit/school', function(req, res) {
+    res.render('school/editableschool');
+  });
+
+  app.get('/users', function(req, res) {
+    res.render('users/users');
+  });
+
+  app.get('/user/:uid', function(req, res) {
+    res.render('users/readonlyuser');
+  });
+
+  app.get('/edit/user/:uid', function(req, res) {
+    res.render('users/editableuser');
+  });
+
+  app.get('/notifications', function(req, res) {
+    res.render('notifications/notifications');
+  });
+
+  app.get('/notification/:uid', function(req, res) {
+    res.render('notifications/readonlynotification');
+  });
+
+  app.get('/edit/notification/:uid', function(req, res) {
+    res.render('notifications/editablenotification');
+  });
 };
